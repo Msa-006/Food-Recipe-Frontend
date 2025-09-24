5757d1bd9997d06a7a7578c081a98d9df07b7703
Msa-006 <mohitkotaru2006@gmail.com>
2025-09-24 19:37:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:9090/foodrecipie',
+        target: 'http://localhost:8081',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
