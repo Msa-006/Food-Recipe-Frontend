023b15ddf2cefa1186d9ffa5c7340dc81599d9fb
Msa-006 <mohitkotaru2006@gmail.com>
2025-09-26 18:56:04 +0530

    Update and rename vite.config.ts to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/FoodRecipe/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8081',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
